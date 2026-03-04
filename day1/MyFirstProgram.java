a59adf6d4531a9cffab4e55c390b4e41d56e3c46
aswathysiva4314-star <aswathy.siva4314@gmail.com>
2026-03-05 00:42:33 +0530

    Add files via upload
    Day1 Assignments

---
+++ day1/MyFirstProgram.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class MyFirstProgram {
+public static void main(String[] args) {
+    System.out.println("Hello");
+    System.out.println("Welcome to TestLeaf");
+    
+}
+    
+}
